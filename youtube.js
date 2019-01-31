f8a055cfadb400a3d73e87aa31ef3f57851498dc
talles <tallesventura@yahoo>
2019-01-31 15:11:29 -0200

    feature: added new attributes for the playerVars

--- youtube.js
+++ youtube.js
@@ -31,7 +31,10 @@ angular.module("info.vietnamcode.nampnq.videogular.plugins.youtube", [])
                             'rel': 0,
                             'autoplay': 0, //Switch autoplay to 1 to autoplay videos
                             'start': 0,
-                            'iv_load_policy': 1
+                            'iv_load_policy': 1,
+                            'cc_load_policy': 0,
+                            'cc_lang_pref': 'pt',
+                            'modestbranding': 0
                         };
 
                         if (optionsArr !== null) {
